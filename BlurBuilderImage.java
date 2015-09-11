54646f450c13d9531a554ab7a9b75e2412953b14
Jovet Alabastro <jovetalabastrodbmanila@gmail.com>
2015-09-11 11:49:05 +0800

    initial commit

---
+++ BlurBuilderImage.java
@@ -0,0 +1,61 @@
+package "your package";
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.renderscript.Allocation;
+import android.renderscript.Element;
+import android.renderscript.RenderScript;
+import android.renderscript.ScriptIntrinsicBlur;
+import android.view.View;
+
+/**
+ * -
+ * Created by :
+ * -
+ * Jovet Alabastro
+ * Android Developer @ DesignBlue Manila Inc.
+ * -
+ * Date created : 8/19/2015 @ 11:59 AM
+ * -
+ **/
+
+
+public class BlurBuilderImage {
+
+    private static final float BITMAP_SCALE = 0.3f;
+    private static final float BLUR_RADIUS = 23.5f;
+
+    public static Bitmap blur(Context context, View view) {
+
+        Bitmap image = getBitmapFromView(view);
+
+        int width = Math.round(image.getWidth() * BITMAP_SCALE);
+        int height = Math.round(image.getHeight() * BITMAP_SCALE);
+
+        Bitmap inputBitmap = Bitmap.createScaledBitmap(image, width, height, false);
+        Bitmap outputBitmap = Bitmap.createBitmap(inputBitmap);
+
+        RenderScript rs = RenderScript.create(context);
+        ScriptIntrinsicBlur theIntrinsic = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs));
+        Allocation tmpIn = Allocation.createFromBitmap(rs, inputBitmap);
+        Allocation tmpOut = Allocation.createFromBitmap(rs, outputBitmap);
+        theIntrinsic.setRadius(BLUR_RADIUS);
+        theIntrinsic.setInput(tmpIn);
+        theIntrinsic.forEach(tmpOut);
+        tmpOut.copyTo(outputBitmap);
+
+        return outputBitmap;
+    }
+
+    private static Bitmap getBitmapFromView(View view) {
+        view.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
+        Bitmap bitmap = Bitmap.createBitmap(view.getMeasuredWidth(), view.getMeasuredHeight(),
+                Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        view.layout(0, 0, view.getMeasuredWidth(), view.getMeasuredHeight());
+        view.draw(canvas);
+        return bitmap;
+    }
+
+}
